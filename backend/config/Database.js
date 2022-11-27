ba5e7c576cbdf3f355aeaf3e8a99a42b1d5c4dd3
loswithaurea <neil.gamboa.chester@gmail.com>
2022-11-27 20:52:03 +0800

    add host

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
  
 const db = new Sequelize('auth_db', 'root', '', {
-    host: "localhost",
+    host: "https://main.d2gxzppztx7e5x.amplifyapp.com/",
     dialect: "mysql"
 });
  
